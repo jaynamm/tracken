1f24f53cfc481411dca9bdd55ab627364cddf437
jave <jave@111percent.net>
2026-09-04 15:35:23 +0900

    Initial Commit

---
+++ tracken/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tracken
+//
+//  Created by jave on 9/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tracken/trackenApp.swift
@@ -0,0 +1,17 @@
+//
+//  trackenApp.swift
+//  tracken
+//
+//  Created by jave on 9/4/26.
+//
+
+import SwiftUI
+
+@main
+struct trackenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
